d4793826569d7fb44a41d958c07b2ce7f9edc0a9
Carl Steele <casteeleu@gmail.com>
2018-07-27 16:15:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+let driver = {}
+function updateDriverWithKeyAndValue(driver, key, value) {
+  let copyDriver = {...driver}
+  copyDriver[key] = value 
+  return copyDriver 
+} 
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  
+
+driver[key] = value;
+return driver 
+}
+
+function deleteFromDriverByKey()
+{
+let copy = {...driver}
+delete copy [key]
+return copy
+}
